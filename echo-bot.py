3d013a3a936ceab51b9698f3854bd613b88365ff
Robert Pavkovich <rpavkovich@gmail.com>
2026-03-06 14:20:12 -0500

    Update echo-bot.py

--- echo-bot.py
+++ echo-bot.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-prompt = st.chat_input("How can I help you..... Type youyr answer here...")
+prompt = st.chat_input("How can I help you, Type youyr answer here...")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
