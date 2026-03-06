8da152ae342235b246ceab3ab8fc436ea8b1c640
Robert Pavkovich <rpavkovich@gmail.com>
2026-03-06 14:05:55 -0500

    Change chat message role from 'police' to 'assistant'

--- echo-bot.py
+++ echo-bot.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
 
-with st.chat_message("police"):
+with st.chat_message("assistant"):
     st.write("You Snooze, You Loose!!! 👋")
+    st.bar_chart(np.random.randn(30, 3))
